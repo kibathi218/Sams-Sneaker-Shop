76e18bddcae585106dc52000692493caec2f880b
Samuel Kibathi <kibathi2180@gmail.com>
2022-03-29 08:31:07 -0400

    styled home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <Navbar />
+      </header>
+      <main>
+      <Routes>
+          <Route path="/" element={ <Home /> } />
+      </Routes>
+      </main>
       
     </div>
   );
---
+++ client/src/data/filler-data.js
@@ -0,0 +1,11 @@
+export default [
+    {id: 1, name: 'Jordan 4 Retro Taupe Haze ', img: '', price: 6000000000},
+    {id: 2, name: 'Nike Air Force One ', img: '', price: 200000},
+    {id: 3, name: 'Nike Vapormax Plus', img: '', description: '', price: 1000000},
+    {id: 4, name: 'Nike LD Waffle Sacai', img: '', description: '', price: 999999},
+    {id: 5, name: 'Jordan 14 Retro Challenge Red', img: '', description: '', price: 15000},
+    {id: 6, name: 'Adidas Superstar 80s', img: '', description: '', price: 3000},
+    {id: 7, name: 'ASICS Gel-Lyte III', img: '', description: '', price: 8000000},
+    {id: 8, name: 'Jordan 1 Retro High Dark Mocha', img: '', description: '', price: 900000},
+  ]
+  
