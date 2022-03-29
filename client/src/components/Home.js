a2787000f11b4f83c27b98024a6b34abfc801e86
Samuel Kibathi <kibathi2180@gmail.com>
2022-03-29 09:06:22 -0400

    added different angles to home page shoe

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,26 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-        <h1>hey</h1>
+        <div className='hero'>
+            <div>
+            <h1>Air Jordan 5 Jade</h1>
+            <img src='https://static.nike.com/a/images/t_prod_ss/w_640,c_limit,f_auto/6c909290-79a8-4c89-9de1-0c6d75d7f717/air-jordan-5-jade-dc7501-300-release-date.jpg' />
+            
+            </div>
+            
+
+            <div className='shoe-angles'>
+            <img style={{width: '200px'}} src='https://static.nike.com/a/images/t_prod_ss/w_640,c_limit,f_auto/6c909290-79a8-4c89-9de1-0c6d75d7f717/air-jordan-5-jade-dc7501-300-release-date.jpg' />
+            <img style={{width: '200px'}} src='https://static.nike.com/a/images/t_prod_ss/w_640,c_limit,f_auto/33eb78b6-8f59-4cac-9b71-d5fd579c47db/air-jordan-5-jade-dc7501-300-release-date.jpg' />
+            <img style={{width: '200px'}} src='https://static.nike.com/a/images/t_prod_ss/w_640,c_limit,f_auto/c8652b63-62f7-4e15-bc28-7c3f52176bd1/air-jordan-5-jade-dc7501-300-release-date.jpg' />
+            <img style={{width: '200px'}} src='https://static.nike.com/a/images/t_prod_sc/w_640,c_limit,f_auto/5136e9ad-91d2-4133-bad2-f53085f698a8/air-jordan-5-jade-dc7501-300-release-date.jpg' />
+            </div>
+            
+        </div>
+        <div className='btns'>
+        <button>click</button>
+        <button>click</button>
+        </div>
     </div>
   )
 }
